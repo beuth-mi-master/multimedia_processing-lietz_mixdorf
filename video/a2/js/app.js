988480c8ab81204a075b0096aba16f0c8e40c50e
Duve, Michael <mduve@designmail.net>
2016-12-29 15:42:00 +0100

    update

--- video/a2/js/app.js
+++ video/a2/js/app.js
@@ -6,24 +6,38 @@ class DrawElement {
         return this.endTime - this.startTime;
     }
 
-    constructor(canvasId, elemId, timeupdateInterval = 0, cb) {
+    constructor(canvasId, elemId, timeupdateInterval = 0, isActive = false, cb) {
         this.canvas = document.getElementById(canvasId);
         this.element = document.getElementById(elemId);
         this.isVideo = this.element instanceof HTMLVideoElement;
         this.container = this.canvas.parentElement;
         this.debugContainer = document.createElement("p");
-        this.debugContainer.classList.add("debug");
-        this.container.appendChild(this.debugContainer);
+        this.debugContainer.classList.add("debug", "col-sm-12", "text-center");
+        this.container.parentElement.appendChild(this.debugContainer);
         this.ctx = this.canvas.getContext('2d');
         this.frameUpdateRequired = false;
+        this.isActive = isActive;
         this.cb = cb;
         this.timeupdateInterval = timeupdateInterval;
+        this.resizeTimeout = null;
         this.init();
     }
 
     init() {
         this.bindEvents();
-        this.element.src = this.element.getAttribute("data-src");
+        let src = this.element.getAttribute("data-src");
+        if (src === "camera") {
+            let _element = this.element;
+            function handleCameraVideo(stream) {
+                _element.src = window.URL.createObjectURL(stream);
+            }
+            function errorCameraVideo(e) {
+                console.log(e);
+            }
+            navigator.getUserMedia({video: true}, handleCameraVideo, errorCameraVideo);
+        } else {
+            this.element.src = src;
+        }
     }
 
     start() {
@@ -36,8 +50,12 @@ class DrawElement {
 
     bindEvents() {
         Helper.addListener(window, "resize orientationchange", () => {
-            this.resize();
-            this.frameUpdateRequired = true;
+            clearTimeout(this.resizeTimeout);
+            this.resizeTimeout = null;
+            this.resizeTimeout = setTimeout(() => {
+                this.resize();
+                this.frameUpdateRequired = true;
+            }, 500);
         });
         if (this.isVideo) {
             Helper.addListener(this.element, "canplay", () => {
@@ -59,7 +77,7 @@ class DrawElement {
     }
 
     draw() {
-        if (this.frameUpdateRequired) {
+        if (this.frameUpdateRequired && this.isActive) {
             this.start();
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             if (!this.isVideo || this.videoIsNotPlaying()) {
@@ -222,6 +240,25 @@ Filter.GRAYSCALE = (frame) => {
     return newFrame;
 };
 
+Filter.SEPIA = (frame) => {
+    const newFrame = new ImageData(frame.width, frame.height);
+    for (let i = 0; i < frame.data.length; i += 4) {
+        const rChannel = i + 0,
+            gChannel = i + 1,
+            bChannel = i + 2,
+            aChannel = i + 3;
+
+        const average = Filter.normalizeGray(frame.data[rChannel], frame.data[gChannel], frame.data[bChannel]);
+
+        newFrame.data[rChannel] = average + 100;
+        newFrame.data[gChannel] = average + 50;
+        newFrame.data[bChannel] = average;
+        newFrame.data[aChannel] = frame.data[aChannel];
+
+    }
+    return newFrame;
+};
+
 Filter.BLUR = (frame, weights = [0, 0, 0, 0, 1, 0, 0, 0, 0]) => {
     const newFrame = new ImageData(frame.width, frame.height);
     const sumOfWeigths = Filter.sum(weights);
@@ -277,29 +314,39 @@ Filter.PREWITT = (frame, weights = [0, 0, 0, 0, 1, 0, 0, 0, 0]) => {
     return newFrame;
 };
 
-let currentFilter = normal;
-
 let drawItems = [
-    new DrawElement('canvas2', 'image1', 0, currentFilter),
-    new DrawElement('canvas1', 'video1', 0, currentFilter)
+    new DrawElement('canvas1', 'imageElement', 0, true, normal),
+    new DrawElement('canvas2', 'videoElement', 0, false, normal),
+    new DrawElement('canvas3', 'cameraElement', 0, false, normal)
 ];
 
-const _GRAYSCALE = new Filter(Filter.GRAYSCALE);
+let currentFilter = normal;
+let currentSource = drawItems[0];
+
+const _GRAYSCALE_FILTER = new Filter(Filter.GRAYSCALE);
+const _SEPIA_FILTER = new Filter(Filter.SEPIA);
 const _BLUR_FILTER = new Filter(Filter.BLUR, [5, 5, 5, 5, 1, 5, 5, 5, 5]);
 const _GAUSS_FILTER = new Filter(Filter.BLUR, [1, 2, 1, 2, 4, 2, 1, 2, 1]);
 const _PREWITT_VERTICAL = new Filter(Filter.PREWITT, [-1, 0, 1, -1, 0, 1, -1, 0, 1]);
 const _PREWITT_HORIZONTAL = new Filter(Filter.PREWITT, [-1, -1, -1, 0, 0, 0, 1, 1, 1]);
 
 function prewitt(frame) {
-    console.log("YEAH");
-    const greyscale = _GRAYSCALE.filterImage(frame);
+    const greyscale = _GRAYSCALE_FILTER.filterImage(frame);
     const prewittVertical = _PREWITT_VERTICAL.filterImage(greyscale);
     const prewittHorizontal = _PREWITT_HORIZONTAL.filterImage(greyscale);
     return Filter.combinePrewitt(prewittVertical, prewittHorizontal);
 }
 
+function grayscale(frame) {
+    return _GRAYSCALE_FILTER.filterImage(frame);
+}
+
 function blur(frame) {
     return _BLUR_FILTER.filterImage(frame);
+}
+
+function sepia(frame) {
+    return _SEPIA_FILTER.filterImage(frame);
 }
 
 function gauss(frame) {
@@ -310,32 +357,68 @@ function normal(frame) {
     return Filter.NORMAL(frame);
 }
 
-function redrawAll() {
-    for (let i = 0; i < drawItems.length; i++) {
-        drawItems[i].cb = currentFilter;
-        drawItems[i].frameUpdateRequired = true;
-    }
+function redraw() {
+    currentSource.cb = currentFilter;
+    currentSource.frameUpdateRequired = true;
 }
 
 const filterDropdown = document.getElementById("filter");
 Helper.addListener(filterDropdown, "change", () => {
     switch (filterDropdown.value) {
-        case "Blur":
+        case "Graustufen":
+            currentFilter = grayscale;
+            break;
+        case "Sepia":
+            currentFilter = sepia;
+            break;
+        case "Weichzeichnung (Mittelwert)":
             currentFilter = blur;
             break;
-        case "Gauss":
+        case "Weichzeichnung (Gauss)":
             currentFilter = gauss;
             break;
-        case "Prewitt":
+        case "Kantendetektion (Prewitt)":
             currentFilter = prewitt;
             break;
         default:
             currentFilter = normal;
             break;
     }
-    redrawAll();
+    redraw();
 });
 
+const imageSection = document.getElementById("image");
+const videoSection = document.getElementById("video");
+const cameraSection = document.getElementById("camera");
+
+let activeSection = imageSection;
+const inputDropdown = document.getElementById("input");
+Helper.addListener(inputDropdown, "change", () => {
+    currentSource.isActive = false;
+    Helper.hide(activeSection);
+    switch (inputDropdown.value) {
+        case "Video":
+            drawItems[2].element.pause();
+            currentSource = drawItems[1];
+            activeSection = videoSection;
+            break;
+        case "Kamera":
+            drawItems[1].element.pause();
+            currentSource = drawItems[2];
+            activeSection = cameraSection;
+            break;
+        default:
+            drawItems[1].element.pause();
+            drawItems[2].element.pause();
+            currentSource = drawItems[0];
+            activeSection = imageSection;
+            break;
+    }
+    Helper.show(activeSection);
+    currentSource.resize();
+    currentSource.isActive = true;
+    redraw();
+});
 
 window.requestAnimFrame = (function() {
     return window.requestAnimationFrame ||
